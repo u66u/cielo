34b3ab8a8e97b76692c99e9083f26285265cec75
u66u <cake3327@gmail.com>
2025-12-16 07:31:04 +0300

    test: densemap file

--- src/common/densemap.rs
+++ src/common/densemap.rs
@@ -116,39 +116,3 @@ impl<I: DenseId, V> FromIterator<(I, V)> for DenseMap<I, V> {
         out
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::DenseMap;
-    use crate::common::ids::{ExprId, VarId};
-
-    #[test]
-    fn densemap_supports_sparse_id_insert_and_lookup() {
-        let mut map = DenseMap::<ExprId, i32>::default();
-        assert!(map.is_empty());
-        assert_eq!(map.len(), 0);
-
-        let _ = map.insert(ExprId::new(5), 10);
-        let _ = map.insert(ExprId::new(1), 20);
-
-        assert_eq!(map.len(), 2);
-        assert_eq!(map.get(&ExprId::new(5)), Some(&10));
-        assert_eq!(map.get(&ExprId::new(1)), Some(&20));
-        assert_eq!(map.get(&ExprId::new(3)), None);
-        assert!(map.contains_key(&ExprId::new(5)));
-        assert!(!map.contains_key(&ExprId::new(3)));
-    }
-
-    #[test]
-    fn densemap_iterates_ids_in_dense_order() {
-        let mut map = DenseMap::<VarId, &'static str>::default();
-        let _ = map.insert(VarId::new(3), "c");
-        let _ = map.insert(VarId::new(1), "a");
-        let _ = map.insert(VarId::new(2), "b");
-
-        let ids = map.iter().map(|(id, _)| id.index()).collect::<Vec<_>>();
-        let values = map.values().copied().collect::<Vec<_>>();
-        assert_eq!(ids, vec![1, 2, 3]);
-        assert_eq!(values, vec!["a", "b", "c"]);
-    }
-}
